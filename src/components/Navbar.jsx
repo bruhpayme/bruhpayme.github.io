05f527197ccfe164ed348e85470159a8a420322a
bruhpayme <shitcode69@gmail.com>
2024-03-16 09:18:04 -0700

    updated sidebar title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 
             </div>
             <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4' >React</h1>
+                <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4' >Pay me.</h1>
 
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-600'>Where's my money?!?!?!</li>
